41930d999adea3ee865b21891c944febec0bc999
Gracieli da Costa Izabel <143838084+gracyizabel@users.noreply.github.com>
2025-08-21 14:18:57 -0300

    Add files via upload

---
+++ js/perguntas.js
@@ -0,0 +1,152 @@
+export const perguntas = [
+    {
+    enunciado: "Você liga a TV e todos os canais falam de uma nova revolução energética: a energia solar se tornou tão eficiente e barata que está mudando o mundo. Qual a sua primeira impressão?",
+        alternativas: [
+            {
+                texto: "Isso é assustador!",
+                afirmacao: [
+                    "No início, ficou com medo das mudanças radicais que essa nova fonte de energia pode trazer para a economia e a sociedade. ",
+                    "Achou assustador pensarque teriam que passar por mudanças radicais"
+                ],
+                proxima:1,
+            },
+            {
+                    texto: "Isso é maravilhoso!",
+                afirmacao: [
+                    "Quis saber como implementar a energia solar em sua casa e comunidade, imaginando um futuro mais limpo e sustentável",
+                    "Pensou que essa nova fonte de energia será muito sustentável"
+                ],
+                proxima: 2,
+            }
+        ]
+    },
+    {
+        enunciado: "Com o avanço das energias renováveis, uma professora de ciências decide fazer uma sequência de aulas sobre a importância da energia sustentável. No fim de uma aula, ela pede que você escreva um trabalho sobre o impacto das energias renováveis no planeta. Qual atitude você toma?",
+        alternativas: [
+            {
+                texto: "Utiliza plataformas de busca na internet para encontrar informações relevantes sobre energia sustentável e como ela pode ser aplicada em diferentes contextos.",
+                afirmacao: [
+                    "Conseguiu utilizar recursos online para pesquisar sobre o tema, descobrindo dados e exemplos de sucesso de energias renováveis",
+                    "Percebeu que a produzir energia de forma sustentavel e muito importante",
+                    "Percebeu a grande importancia da energia sustentável "
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Escreve o trabalho com base em conversas com colegas, algumas pesquisas em livros e conhecimentos próprios sobre o tema",
+                afirmacao:[
+                     "Sentiu mais facilidade em utilizar seus próprios recursos e conhecimentos prévios para elaborar seu trabalho sobre energias limpas.",
+                     "Achou que era muito mais fácil procurar por respostas utilizando alguns meios mais tradicionais mesmo que levasse mais tempo.",
+                     "Sentiu um pouco de medo de quais dados pessoais seus a IA poderia utilizar e por isso prefere fazer suas coisas com pouca intromissão da tecnologia."
+                ],
+                proxima: 4,
+        
+            },
+        ]
+    },
+    {
+        enunciado: "Após a elaboração do trabalho escrito, a professora promoveu um debate em sala para entender as pesquisas e discutir um ponto crucial: como as energias renováveis impactam o mercado de trabalho do futuro. Nesse debate, como você se posiciona?",
+        alternativas: [
+            {
+                texto: "Defende a ideia de que a transição para energias renováveis pode criar novas oportunidades de emprego e requalificar a força de trabalho",
+                afirmacao: [
+                 "Vem impulsionando a inovação no setor de energia sustentável e luta para abrir novos caminhos profissionais na área verde.",
+                 "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processo da nova conquista."
+            
+            ],
+            proxima: 5,
+            },
+            {
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                afirmacao: [
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Utilizar informações próprias traz um toque pessoal ao nosso trabalho. Agora imagine que você precisa participar de um debate sobre o quanto a IA impacta no trabalho do futuro. Qual posicionamento tomar?",
+        alternativas: [
+            {
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                afirmacao: [
+                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                afirmacao: [
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Novas tecnologias trazem novos desafios, por isso após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        alternativas: [
+            {
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                afirmacao: [
+                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                afirmacao: [
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "É muito importante refletir sobre os trabalhadores do futuro. Por isso a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        alternativas: [
+            {
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                afirmacao: [
+                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                afirmacao: [
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        alternativas: [
+            {
+                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
+                afirmacao: [
+                    "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes.",
+                    "Ainda acha que os meios de desenho tradicionais são mais eficazes para a criatividade, por isso vem estimulando pessoas em suas redes sociais a fazer pintura em aquarela."
+                ],
+            },
+            {
+                texto: "Criar uma imagem da nutureza utilizando um gerador de imagem de IA.",
+                afirmacao: [
+                    "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!",
+                    "Compartilhou artes em redes sociais como forma de ensinar como se comunicar através da arte.",
+                    "Percebeu que muitas pessoas têm dificuldade em expressar suas ideias desenhando e acha que a inspiração da natureza é capaz de empoderar essas pessoas a tirarem ideias do papel."
+                ],
+            },
+        ]
+    }
+];
